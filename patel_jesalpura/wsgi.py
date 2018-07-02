02be196ba5fb1e783d7f6025deefccffce54225e
Alok Kumar <alokyadav495@gmail.com>
2018-07-02 16:58:18 +0530

    setting chnaged

--- patel_jesalpura/wsgi.py
+++ patel_jesalpura/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "patel_jesalpura.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "patel_jesalpura.settings")
 
 application = get_wsgi_application()
